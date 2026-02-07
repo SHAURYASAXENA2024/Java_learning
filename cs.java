4bd065c12adb5ddbcf5e21fa4317cbacfc3125cc
Shaurya Saxena <shauryasaxena836@gmail.com>
2026-02-08 01:34:36 +0530

    java assignment

---
+++ cs.java
@@ -0,0 +1,15 @@
+class cs{
+    // USE OF THIS POINTER(POINTER DOES NOT EXIST IN JAVA THERE IS REFERNCE IN JAVA)
+    int num1,num2;
+    void get(int num1 , int num2){
+        this.num1=num1;
+        this.num2=num2;
+    }
+    void show(){
+        System.out.println(num1);
+        System.out.println(num2);
+    }
+    public static void main(String[] args){
+            
+        }
+    }
